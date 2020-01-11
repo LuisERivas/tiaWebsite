44151d637bdae8816c7a307efd2e5feb4bfb220b
LuisERivas <lucho.rivas.villacorta@gmail.com>
2020-01-11 12:43:10 -0800

    added banner image and slots for product previews

---
+++ app/src/Components/HomeBannerImage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './index.css'
+import HomePageBannerImage from '../HomePageBannerImageAsset/HomePageBannerImage01.jpg'
+
+class HomeBannerImage extends Component {
+  render () {
+    return (
+      <div className='bg-light'>
+        <img src={HomePageBannerImage} className='homeBannerSize' alt='banner Image' />
+      </div>
+    )
+  }
+}
+
+export default HomeBannerImage
--- app/src/Components/Homepage/index.js
+++ app/src/Components/Homepage/index.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 import './index.css'
+import HomePageProductDisplayCard from '../HomePageProductDisplayCard'
 
 class Homepage extends Component {
   render () {
     return (
-      <div className='bg-dark '>
-        <div className='container p-0 m-0'>
+      <div className='bg-dark'>
+        <div className='container pt-3'>
           <div className='col bg-primary'>
             <div className='row'>
               <div className='col'>
-        Home Page
-
+                <HomePageProductDisplayCard />
+              </div>
+              <div className='col'>
+                <HomePageProductDisplayCard />
+              </div>
+              <div className='col'>
+                <HomePageProductDisplayCard />
+              </div>
+              <div className='col'>
+                <HomePageProductDisplayCard />
               </div>
             </div>
+          </div>
+        </div>
+        <div className=''>
+          <div className=' bg-danger'>
+            ugly boi
           </div>
         </div>
       </div>
