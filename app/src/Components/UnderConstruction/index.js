b5217ff9dc7fb42c573d7805c6ab13627e5267cd
LuisERivas <lucho.rivas.villacorta@gmail.com>
2020-01-13 12:28:49 -0800

    finished up procedure display component with links

---
+++ app/src/Components/UnderConstruction/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './index.css'
+
+class UnderConstruction extends Component {
+  render () {
+    return (
+      <div className='container bg-danger'>
+        <h1>DONT PANIC!</h1>
+        <p>We're just under Construction =D</p>
+      </div>
+    )
+  }
+}
+
+export default UnderConstruction
